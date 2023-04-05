c621d3a1ce40ac9631cc709631306354090cd83a
BMO <117410264+beaunaturale@users.noreply.github.com>
2023-04-04 22:51:17 -0500

    added second API request

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ var APIKey = "0ec00862a300db548b59bba29a37fead";
 
 var cityFormEl = $('#city-form')
 var cityListEl = $('#city-list')
-var orderCity = 'cleveland';
+var inputCity = document.querySelector("input");
 
 function getApi() {
   // var queryUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={0ec00862a300db548b59bba29a37fead}"
-  var geoUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + orderCity + '&appid=' + APIKey
+  var geoUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + inputCity + '&appid=' + APIKey
 
   fetch(geoUrl)
     .then(function (response) {
@@ -15,10 +15,14 @@ function getApi() {
     })
     .then(function(geoResults) {
       console.log(geoResults);
+
+    let latitude = geoResults[0].lat;
+    let longitude = geoResults[0].lon;
       // do whatever w/ the results
       // extracting the data you want
       // using that data to make a new url(5day forecast)
       // making another api call
+    var queryUrl = 'api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + APIKey
       // -- then
       // -- extract that new data
       // -- shove it on the screen
