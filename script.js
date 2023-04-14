52240ecf8f2b7fb15148311d2e62d0afb67dfcb5
BMO <117410264+beaunaturale@users.noreply.github.com>
2023-04-13 22:33:35 -0500

    current weather card info added

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 var APIKey = "0ec00862a300db548b59bba29a37fead";
-
+var city = []
 var cityFormEl = $('#city-form')
 var cityListEl = $('#city-list')
 var searchBtnEl = $('#searchBtn')
 var inputCity = document.querySelector("input");
+var currentCard = document.querySelector("#current-weather");
+
+
+// var listedCity = function() {
+//   console.log("saved cities list")
+//   localStorage.setItem('city', JSON.stringify(city))
+// }
 
 function getApi() {
-  // var newGeoUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={0ec00862a300db548b59bba29a37fead}"
+  // var newWeatherURL = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={0ec00862a300db548b59bba29a37fead}"
   var cityValue = inputCity.value;
 
   var geoUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityValue + '&appid=' + APIKey
@@ -18,26 +25,69 @@ function getApi() {
     })
     .then(function(geoResults) {
       console.log(geoResults);
-
-    let latitude = geoResults[0].lat;
-    let longitude = geoResults[0].lon;
       // do whatever w/ the results
       // extracting the data you want
+    let latitude = geoResults[0].lat;
+    let longitude = geoResults[0].lon;
       // using that data to make a new url(5day forecast)
       // making another api call
-    var newGeoUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + APIKey
+    var newWeatherURL = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + APIKey
 
-    fetch(newGeoUrl)
+    fetch(newWeatherURL)
       .then(function (response) {
         console.log(response);
         return response.json();
       })
-      .then(function(newGeoResults) {
-        console.log(newGeoResults);
+      .then(function(newWeatherResults) {
+        console.log(newWeatherResults);
+
+        
+        currentCard.innerHTML = ""
+        //this is the data for the current day 
+        var currentDay  = newWeatherResults.list[0]
+        var currentTime = new Date(currentDay.dt * 1000).toLocaleDateString()
+        var currentTemp = Math.floor((parseInt(currentDay.main.temp) - 273.15) * 9 / 5 + 32) + 'F'
+        var currentWind = currentDay.wind.speed
+        var currentHum = currentDay.main.humidity
+        console.log(currentTime, currentTemp, currentWind, currentHum)
+        
+        //This is creating the current day card element
+        var currentTimeEl = document.createElement("p")
+        currentTimeEl.textContent = ""
+        currentTimeEl.textContent = `Current Day : ${currentTime}`
+        
+        var currentTempEl = document.createElement("p")
+        currentTempEl.textContent = ""
+        currentTempEl.textContent = `Current Temperature : ${currentTemp}`
+        
+        var currentWindEl = document.createElement("p")
+        currentWindEl.textContent = ""
+        currentWindEl.textContent = `Current Wind: ${currentWind}`
+        
+        var currentHumEl = document.createElement("p")
+        currentHumEl.textContent = ""
+        currentHumEl.textContent = `Current Humidity : ${currentHum}`
+
+        currentCard.append(currentTimeEl)
+        currentCard.append(currentTempEl)
+        currentCard.append(currentWindEl)
+        currentCard.append(currentHumEl)
+
+        console.log(currentCard)
+
+
+
+
+        //TODO: CREATE elements for 5 day forecast 
+        //console.log(new Date(currentTime * 1000).toLocaleDateString())
+        // -- then
+        // -- extract that new data
+      // let temperature = newWeatherResults[0].temp;
+      // let windSpeed = newWeatherResults[0].wind;
+      // let humidity = newWeatherResults[0].humidity;
+      //   console.log(temperature, windSpeed, humidity)
 
       }) 
-      // -- then
-      // -- extract that new data
       // -- shove it on the screen
     })
 
@@ -72,6 +122,11 @@ function handleFormSubmit(event) {
 
 // on click - run this function
 searchBtnEl.click(handleFormSubmit)
+
+localStorage.setItem(cityListEl, inputCity)
+
+let cityList = localStorage.getItem(inputCity)
+
 
 
 
